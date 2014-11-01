2d377abd3f745f3f3ff405962cda2b790a74ce4b
darren-gibson <darren-gibson@ntlworld.com>
2014-11-01 08:10:23 +0000

    Add untracted file

---
+++ CustomerOrder.Contract/DTO/Money.cs
@@ -0,0 +1,28 @@
+﻿namespace CustomerOrder.Contract.DTO
+{
+    using Annotations;
+    using Model;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public class Money
+    {
+        public Money() { }
+        public Money(Model.Money money)
+        {
+            Amount = decimal.Parse(money.ToString("N", null));
+            Currency = money.Code;
+        }
+
+        [JsonProperty(PropertyName = "amount")]
+        public decimal Amount { get; set; }
+        [JsonProperty(PropertyName = "currency"), JsonConverter(typeof(StringEnumConverter))]
+        public Currency Currency { get; set; }
+
+        public Model.Money ToMoney()
+        {
+            return new Model.Money(Currency, Amount);
+        }
+    }
+}
