cd35cdfb2ed3727676d68d8dda2df4f5c61f87d5
unknown <xa10@UKLTD16784.global.tesco.org>
2014-11-01 16:34:08 +0000

    Create the amount due and the amount paid at the order level and assoicated properties within the totals on the GetOrder response.  Rework the order so that it has a deined currency and that is used by the SimplePriceService.

---
+++ CustomerOrder.Model/CurrencyDoesNotMatchOrderException.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerOrder.Model
+{
+    using System;
+
+    public class CurrencyDoesNotMatchOrderException : Exception
+    {
+        public CurrencyDoesNotMatchOrderException(Currency orderCurrency, Currency requestedCurrency) : 
+            base(string.Format("Currency {0} does not match order currency of {1}", requestedCurrency, orderCurrency))
+        {
+            
+        }
+    }
+}
