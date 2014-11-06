4318f8062a81d7ae820d2e5e75bff68b6756f6de
darren-gibson <darren-gibson@ntlworld.com>
2014-11-06 07:42:03 +0000

    Add error function to the growl notificaton

--- TillDemoApp/app/services/notify.js
+++ TillDemoApp/app/services/notify.js
@@ -7,6 +7,10 @@ angular.module('tillDempApp').factory('notify', ['growl', function (growl) {
         growl.warning(message, { "ttl": 10000, title: title, "variables": variables });
     };
 
+    service.error = function (title, message, variables) {
+        growl.error(message, { "ttl": 10000, title: title, "variables": variables });
+    };
+
     service.success = function(title, message, variables) {
         growl.success(message, { "ttl": 3000, title: title, "variables": variables });
     };
